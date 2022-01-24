0b22fae67f8ac7640d7a16dd496162be4b6124da
Risenes <83907051+Risenes@users.noreply.github.com>
2022-01-24 22:13:34 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -168,7 +168,7 @@ function getValue() {
 
 
 	let rawFile = new XMLHttpRequest();
-	rawFile.open("GET", "letter2.0/table.csv", true);
+	rawFile.open("GET", "../letter2.0/table.csv", true);
 	rawFile.onreadystatechange = function () {
 		if (rawFile.readyState === 4) {
 			if (rawFile.status === 200 || rawFile.status == 0) {
