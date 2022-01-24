470e9c1a8c6fa3a19e601d0f32eee90cdd98896b
Risenes <83907051+Risenes@users.noreply.github.com>
2022-01-24 21:55:25 +0700

    fix js

--- script/script.js
+++ script/script.js
@@ -67,7 +67,7 @@
 // }
 
 // var rawFile = new XMLHttpRequest();
-// rawFile.open("GET", "../table.csv", true);
+// rawFile.open("GET", "../../table.csv", true);
 // rawFile.onreadystatechange = function () {
 //   if (rawFile.readyState === 4) {
 //     if (rawFile.status === 200 || rawFile.status == 0) {
@@ -168,7 +168,7 @@ function getValue() {
 
 
 	let rawFile = new XMLHttpRequest();
-	rawFile.open("GET", "../table.csv", true);
+	rawFile.open("GET", "../../table.csv", true);
 	rawFile.onreadystatechange = function () {
 		if (rawFile.readyState === 4) {
 			if (rawFile.status === 200 || rawFile.status == 0) {
