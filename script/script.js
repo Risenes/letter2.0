df13e601c14c6ea1075841a9c7e14594fb194b2f
Risenes <83907051+Risenes@users.noreply.github.com>
2022-01-31 20:53:22 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -68,7 +68,7 @@ function getValue() {
 				console.log(resHello)
 				let helloR = checkText(resHello);
 				console.log(helloR)
-				document.getElementById('letter__hello').innerHTML = resHello + " いつも　お世話に　なって　おります。";
+				document.getElementById('letter__hello').innerHTML = helloR + " いつも　お世話に　なって　おります。";
 
 				let senseiHealth = result.map(x => x.ЗдоровьеСенсея).filter(String)
 				let SHR = checkText(senseiHealth)
